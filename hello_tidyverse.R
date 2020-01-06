a6b3c3c9487fdec1f565e2bd1534f735f858e0ab
AA373134 <aa373134@ee37313acbookea.dhcp.hakozaki.ibm.com>
2020-01-06 19:31:57 +0900

    男女による予測モデル作成

--- hello_tidyverse.R
+++ hello_tidyverse.R
@@ -1,3 +1,22 @@
-# コメント付加
-tidyverse::tidyverse_logo()
-# aaaaaaaaa
+## ライブラリの呼び出し
+
+library(tidyverse)
+
+## データの読み込み
+
+train <- read_csv("input/train.csv")
+test <- read_csv("input/test.csv")
+
+## 基礎集計
+
+table(train$Survived)
+table(train$Survived, train$Sex)
+
+## 男性は0、女性は1としてルールベースで予測
+
+ans_1 <- test %>% 
+  select(PassengerId, Sex) %>% 
+  mutate(Servived = ifelse(Sex == "female", 1, 0)) %>% 
+  select(-Sex) 
+write_csv(ans_1, "output/ans_1.csv")
+  
